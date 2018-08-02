b4cbdc99b50571afa6751945d84ff6a60686a9b4
ajoshpratt@gmail.com <ajoshpratt@gmail.com>
2018-08-02 09:31:52 -0400

    Adding in reservation.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
 # Starts an interactive session on H2P
-crc-interactive.py -p compbio -m --time=1 --num-cores=64
+crc-interactive.py -p compbio -m --time=1 --num-cores=64 --account=westpa_2018S
